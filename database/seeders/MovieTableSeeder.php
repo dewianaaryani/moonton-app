423ae07c544ff5fb48bc74fa4af253d486bdf947
dewianaaryani <dewianarahmat@gmail.com>
2022-07-26 18:41:02 +0700

    feat: integration dashboard

--- database/seeders/MovieTableSeeder.php
+++ database/seeders/MovieTableSeeder.php
@@ -16,12 +16,12 @@ class MovieTableSeeder extends Seeder
     {
         $movies = [
             [
-                'name' => 'The Shawshank Redemption',
-                'slug' => 'the-shawshank-redemption',
-                'category' => 'Drama',
-                'video_url' => 'https://www.youtube.com/watch?v=46-Ueij5Q5k',
-                'thumbnail' => 'https://pict.sindonews.net/dyn/850/pena/news/2022/04/11/700/739723/profil-6-member-le-sserafim-dan-kontroversinya-jvb.jpg',
-                'rating' => 9.3,
+                'name' => 'Incantation',
+                'slug' => 'incantation',
+                'category' => 'Horror',
+                'video_url' => 'https://www.youtube.com/watch?v=HLp1qWDXzPM',
+                'thumbnail' => 'https://akcdn.detik.net.id/visual/2022/07/13/incantation-3_169.png?w=650',
+                'rating' => 5.0,
                 'is_featured' => 1,
             ],
             [
@@ -30,7 +30,7 @@ class MovieTableSeeder extends Seeder
                 'category' => 'Drama',
                 'video_url' => 'https://www.youtube.com/watch?v=apTcyOsQ2rw',
                 'thumbnail' => 'https://1.bp.blogspot.com/-lUy88adMwmA/XqpzQbcKouI/AAAAAAAABes/TKknYOgFnLo-MYvCfUaj4Ds7sl_B-xLmwCLcBGAsYHQ/s1600/irene%2Bred%2Bvelvet%2Bmenikah%2Bpacar%2Bsuami.jpg',
-                'rating' => 9.9,
+                'rating' => 4,
                 'is_featured' => 0,
             ],
             [
@@ -38,8 +38,8 @@ class MovieTableSeeder extends Seeder
                 'slug' => 'Pirate-of-the-Caribbean',
                 'category' => 'Fantasy',
                 'video_url' => 'https://www.youtube.com/watch?v=Hgeu5rhoxxY',
-                'thumbnail' => 'hhttps://upload.wikimedia.org/wikipedia/en/5/5e/Pirates_of_the_Caribbean_-_On_Stranger_Tides.png',
-                'rating' => 9.9,
+                'thumbnail' => 'https://upload.wikimedia.org/wikipedia/en/5/5e/Pirates_of_the_Caribbean_-_On_Stranger_Tides.png',
+                'rating' => 3,
                 'is_featured' => 0,
             ],
         ];
--- resources/js/Layouts/Authenticated/Topbar.jsx
+++ resources/js/Layouts/Authenticated/Topbar.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
-export default function Topbar(){
+import { Link } from "@inertiajs/inertia-react";
+export default function Topbar({name}){
     const [dropdownOpen, setDropdownOpen] = useState(true)
     const dropdownTarget = useRef();
 
@@ -16,7 +17,7 @@ export default function Topbar(){
         <div className="flex justify-between items-center cursor-pointer">
             <input type="text" className="top-search" placeholder="Search movie, cast, genre" />
             <div className="flex items-center gap-4">
-                <span className="text-black text-sm font-medium">Welcome, Granola Sky</span>
+                <span className="text-black text-sm font-medium">Welcome, {name}</span>
                 {/* <!-- user avatar --> */}
                 <div className="collapsible-dropdown flex flex-col gap-2 relative">
                     <div
@@ -27,7 +28,7 @@ export default function Topbar(){
                         ref={dropdownTarget}>
                         <a href="#!" className="transition-all hover:bg-sky-100 p-4">Dashboard</a>
                         <a href="#!" className="transition-all hover:bg-sky-100 p-4">Settings</a>
-                        <a href="sign_in.html" className="transition-all hover:bg-sky-100 p-4">Sign Out</a>
+                        <Link href={route('logout')} method="post" className="transition-all hover:bg-sky-100 p-4">Sign Out</Link>
                     </div>
                 </div>
             </div>
